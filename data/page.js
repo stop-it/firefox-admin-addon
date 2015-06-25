cba6178824d5e993d341175127d313bf3ebca83d
ondrejd <ondrejd@gmail.com>
2015-06-25 15:25:42 +0200

    Improved main add-on page. Added database file preference. Styles improved.

--- data/page.js
+++ data/page.js
@@ -2,12 +2,46 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Triggered when page is loaded.
-addon.port.on('load', function onShow() {
-	console.log('[page].onLoad');
-});
+// This is triggered when error is occured.
+self.port.on('error', function(type) {
+	// Database connection is not established.
+	if (type == 'database_connection_undefined') {
+		var errorDlg = document.createElement('div');
+		errorDlg.classList.add('dialog');
+		errorDlg.classList.add('errorDialog');
 
-// Triggered when page is going to be unloaded.
-addon.port.on('unload', function onHide() {
-	console.log('[page].onUnload');
+		var header = document.createElement('header');
+		var h2 = document.createElement('h2');
+		h2.innerHTML = 'Database connection error';
+		header.appendChild(h2);
+
+		var content = document.createElement('div');
+		content.classList.add('content');
+		var p = document.createElement('p');
+		p.innerHTML = 'Unable to connect <b>Stop-It</b> SQLite database. ' + 
+			'Open add-on preferences and check if path to database file ' + 
+			'is set correctly.';
+		content.appendChild(p);
+
+		var footer = document.createElement('footer');
+		var buttonClose = document.createElement('button');
+		buttonClose.setAttribute('type', 'button');
+		buttonClose.innerHTML = 'Close page';
+		buttonClose.addEventListener('click', function(event) {
+			self.port.emit('closePage');
+		});
+		footer.appendChild(buttonClose);
+
+		//var buttonPrefs = document.createElement('button');
+		//buttonPrefs.setAttribute('type', 'button');
+		//buttonPrefs.innerHTML = 'Open preferences';
+		//footer.appendChild(buttonPrefs);
+
+		errorDlg.appendChild(header);
+		errorDlg.appendChild(content);
+		errorDlg.appendChild(footer);
+
+		var pageContent = document.getElementById('content');
+		pageContent.appendChild(errorDlg);
+	}
 });
